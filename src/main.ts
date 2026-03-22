dc904a496f6ecb4851a44ae3a0e8510fd7d51bd0
Almog Bokobza <almogishot@gmail.com>
2026-03-22 21:57:40 +0200

    Revert "Add dotenv"
    This reverts commit ad55d69b6f18fc391704743d540360f15034e795.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
-import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env)
+  
   const port = process.env.PORT;
+  
   await app.listen(Number(port));
   console.log(`Application is running on: http://localhost:${port}`);
 }
