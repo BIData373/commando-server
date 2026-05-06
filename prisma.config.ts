8ef2c5b4ee9804489b07e2b1d841a62e330032f9
Nir Finkleshtein <s9336069@idfts.il>
2026-05-06 19:51:55 +0300

    Fix env for deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,11 +8,11 @@ if (!process.env.ENV) {
 
 config({ path: path.join('./config', process.env.ENV, '.env') })
 
-const host = process.env.PG_HOST
+const host = process.env.PGHOST
 const port = process.env.PG_PORT
-const username = process.env.PG_USERNAME
-const password = process.env.PG_PASSWORD
-const database = process.env.PG_DATABASE
+const username = process.env.PGUSER
+const password = process.env.PGPASSWORD
+const database = process.env.POSTGRES_DATABASE
 
 const url = `postgres://${username}:${password}@${host}:${port}/${database}`
 
