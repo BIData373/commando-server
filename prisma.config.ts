440b659923ca96f9447744d51951dca300c026cb
rocketdv <leviu02@gmail.com>
2026-05-12 15:29:54 +0300

    feat: add in postinstall generate the prisma client

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,13 @@ import { config } from 'dotenv';
 import path from 'node:path';
 import { defineConfig } from "prisma/config";
 
-if (!process.env.ENV) {
-  throw new Error('ENV was not passed!')
-}
+// if (!process.env.ENV) {
+//   throw new Error('ENV was not passed!')
+// }
 
-config({ path: path.join('./config', process.env.ENV, '.env') })
+if (process.env.ENV) {
+  config({ path: path.join('./config', process.env.ENV, '.env') })
+}
 
 const url = process.env.DATABASE_URL
 
