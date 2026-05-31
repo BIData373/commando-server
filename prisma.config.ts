f6352a7363e708e143dc17f912af6f7b3b431165
Nir Finkleshtein <s9336069@idfts.il>
2026-05-31 16:01:35 +0300

    fix: updated env examples

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,20 +7,12 @@ if (process.env.ENV) {
   config({ path: path.join('./config', process.env.ENV, '.env') })
 }
 
-const host = process.env.PGHOST
-const port = process.env.PG_PORT
-const username = process.env.PGUSER
-const password = process.env.PGPASSWORD
-const database = process.env.POSTGRES_DATABASE
-
-const url = process.env.DATABASE_URL ?? `postgres://${username}:${password}@${host}:${port}/${database}`
-
 export default defineConfig({
   schema: "src/entities/",
   migrations: {
     path: "migrations/",
   },
   datasource: {
-    url
+    url: process.env.DATABASE_URL
   },
 });
