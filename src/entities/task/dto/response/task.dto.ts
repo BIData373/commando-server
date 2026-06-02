8f1ca4b994f01484c82ceb3243be07995b86ab2c
rocketdv <leviu02@gmail.com>
2026-06-02 20:17:09 +0300

    fix: task dto type

--- src/entities/task/dto/response/task.dto.ts
+++ src/entities/task/dto/response/task.dto.ts
@@ -1,10 +1,10 @@
 import { Exclude, Type } from 'class-transformer';
 import { ExposeProperty } from '../../../../common/decorators/expose-property.decorator';
 import { IdMetaFieldsDto } from '../../../../common/dto/response/id-meta-fields.dto';
+import { DeadlineType } from '../../../../types/prisma';
 import { SourceDto } from '../../../source/dto/response/source.dto';
 import { TagDto } from '../../../tag/dto/response/tag.dto';
 import { AssigneeStatusDto } from './assignee-status.dto';
-import { DeadlineType } from '../../../../types/prisma';
 
 @Exclude()
 export class TaskDto extends IdMetaFieldsDto {
@@ -32,9 +32,9 @@ export class TaskDto extends IdMetaFieldsDto {
   @ExposeProperty()
   workspaceId: number;
 
-  @ExposeProperty()
+  @ExposeProperty({ type: SourceDto, nullable: true })
   @Type(() => SourceDto)
-  source: SourceDto;
+  source: SourceDto | null;
 
   @ExposeProperty({ type: [TagDto] })
   @Type(() => TagDto)
