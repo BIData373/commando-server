30691d8544319489def045ea01eeba47bc9b22d3
Nir Finkleshtein <s9336069@idfts.il>
2026-06-14 12:18:19 +0300

    format upn correctly

--- src/common/functions/user.ts
+++ src/common/functions/user.ts
@@ -1,13 +1,5 @@
 export const UPN_SUFFIX = '@idf.il'
 
-export function removeUpnSuffix(upn: string) {
-  return upn.split('@')[0]
-}
-
-export function addUpnSuffix(upn?: string) {
-  return upn && (removeUpnSuffix(upn) + UPN_SUFFIX)
-}
-
-export function formatUpnForEntity(upn?: string) {
-  return addUpnSuffix(upn)?.trim().toLowerCase()
+export function formatUpnForEntity(upn: string) {
+  return upn.split('@')[0]?.trim().toLowerCase()
 }
