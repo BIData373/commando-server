68561e027223c8157d0e3c82303725e695ee60b5
Leo Ukraincik <45918762+rocketdv@users.noreply.github.com>
2026-06-15 15:39:52 +0300

    Almog/deploy to ts (#17)
    * Push to origin
    
    * temp
    
    * Update
    
    * fix: depth 1
    
    * chore: fixes to the docker files
    
    * GET cookies in mock sso
    
    * Feature: Mirage Search (#15)
    
    * fix: seed + workspaceId in default status search
    
    * feat: mirage search
    
    g
    
    * chore: resolve todo
    
    * chore: resolve another todo
    
    * fix: user-info dto
    
    * fix: undefined types
    
    * chore: var order
    
    * fix: user adds upn properly
    
    * chore: try to fix
    
    * Allow GET permission for all users
    
    * format upn correctly
    
    ---------
    
    Co-authored-by: Almog Bokobza <almogishot@gmail.com>
    Co-authored-by: Nir Finkleshtein <s9336069@idfts.il>

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
-docker rm -f server
-docker rmi server
+if ! docker info >/dev/null 2>&1; then
+    echo "Error: Docker daemon is not running (or you don't have permission to take to it)" >&2
+    exit 1
+fi
 
-docker build -t server --platform linux/amd64 .
-docker run --env-file .env --name server -d -p 3000:3000 server
+REPO_NAME=commando-server
+IMAGE_NAME=server
+CONTAINER_NAME=temp_server
+CONTAINER_FILE_PATH="/home/app/server.tar.gz"
+
+rm -rf $REPO_NAME
+
+git clone git@github.com:BIData373/$REPO_NAME.git --depth=1
+cd $REPO_NAME
+git checkout dev
+
+cp ../zip.dockerfile .
+
+docker rm -f $CONTAINER_NAME
+docker rmi -f $IMAGE_NAME
+docker build -f zip.dockerfile -t $IMAGE_NAME --platform linux/amd64 .
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+docker cp $CONTAINER_NAME:$CONTAINER_FILE_PATH ../
+docker rm -f $CONTAINER_NAME
+docker rmi -f $IMAGE_NAME
+
+rm -rf ../$REPO_NAME
