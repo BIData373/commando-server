797e5633ebea401e4e1cc6828964b591d4238a08
Nir Finkleshtein <s9336069@idfts.il>
2026-07-16 19:33:46 +0300

    Fix findPersonalRows filtering based on current user

--- src/entities/task/task.service.ts
+++ src/entities/task/task.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { keyBy, map, uniq } from 'lodash';
+import { renderTemplate } from '../../common/functions/template';
 import { PrismaService } from '../../common/prisma.service';
 import { PermissionType, Prisma, Task, User, WorkspaceStatus } from '../../types/prisma';
+import { MessageRelayService } from '../services/message-relay.service';
 import { WorkspaceWithPermissions } from '../workspace/types/workspace-with-permission.type';
 import { CreateTaskDto } from './dto/request/create-task.dto';
 import { UpdateTaskDto } from './dto/request/update-task.dto';
-import { MessageRelayService } from '../services/message-relay.service';
-import { renderTemplate } from '../../common/functions/template';
 
 type AssigneeStatusInclude = {
   include: { assignee: { include: { users: true } }; status: true };
@@ -41,13 +41,10 @@ export class TaskService {
       assigneeStatuses: {
         where: { assignee: { deletedAt: null } },
         orderBy: { assigneeId: 'asc' },
+        ...(userId && { where: { assignee: { users: { some: { id: userId } } } } }),
         include: {
           assignee: {
-            include: {
-              users: userId
-                ? { where: { id: userId } }
-                : true
-            }
+            include: { users: true }
           },
           status: true
         }
@@ -55,9 +52,9 @@ export class TaskService {
     } satisfies Prisma.TaskInclude
   }
 
-  static withWorkspaceInclude(userId?: number) {
+  static withWorkspaceInclude(userId?: number, filterAssignees: boolean = false) {
     return {
-      ...TaskService.baseInclude(userId),
+      ...TaskService.baseInclude(filterAssignees ? userId : undefined),
       workspace: {
         include: {
           permissions: userId
@@ -295,7 +292,7 @@ export class TaskService {
         assigneeStatuses: { some: { assignee: { users: { some: { id: user.id } } } } },
         deletedAt: null
       },
-      include: TaskService.withWorkspaceInclude(user.id),
+      include: TaskService.withWorkspaceInclude(user.id, true),
       orderBy: TaskService.orderBy
     });
   }
