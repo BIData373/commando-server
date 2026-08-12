eeb193eb6580ed58cc57049861d63bec93d90aa5
karnishitrit <karniishitrit@gmail.com>
2026-08-12 13:29:21 +0300

    change the status colors to lighter

--- src/entities/workspace-status/consts/default-statuses.ts
+++ src/entities/workspace-status/consts/default-statuses.ts
@@ -1,7 +1,12 @@
 import { Prisma, WorkspaceStatusType } from "../../../types/prisma";
 
-export const DEFAULT_STATUSES: Prisma.WorkspaceStatusCreateManyWorkspaceInput[] = [
-  { name: 'טרם בוצע', color: '#FA541C', type: WorkspaceStatusType.NOT_STARTED },
-  { name: 'בעבודה', color: '#2F54EB', type: WorkspaceStatusType.IN_PROGRESS },
-  { name: 'בוצע', color: '#52C41A', type: WorkspaceStatusType.COMPLETED }
-] as const
+export const DEFAULT_STATUSES: Prisma.WorkspaceStatusCreateManyWorkspaceInput[] =
+  [
+    {
+      name: "טרם בוצע",
+      color: "#FF9C6E",
+      type: WorkspaceStatusType.NOT_STARTED,
+    },
+    { name: "בעבודה", color: "#85A5FF", type: WorkspaceStatusType.IN_PROGRESS },
+    { name: "בוצע", color: "#95DE64", type: WorkspaceStatusType.COMPLETED },
+  ] as const;
