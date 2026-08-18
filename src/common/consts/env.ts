b01124e8e5f8635156ba2223ed7463d0e2f7deef
Nir Finkleshtein <s9336069@idfts.il>
2026-08-18 19:35:58 +0300

    Fix runtime vague error on missing env

--- src/common/consts/env.ts
+++ src/common/consts/env.ts
@@ -77,4 +77,6 @@ export const mirageUrl = process.env.MIRAGE_URL
 export const mirageKey = process.env.MIRAGE_KEY
 export const mirageVersion = process.env.MIRAGE_VERSION
 
-export const projectChatUrl = new URL(`/channel/${process.env.VITE_CHAT_CHANNEL!}`, process.env.VITE_CHAT_URL!).href
+export const projectChatUrl = process.env.VITE_CHAT_URL && process.env.VITE_CHAT_CHANNEL
+  ? new URL(`/channel/${process.env.VITE_CHAT_CHANNEL}`, process.env.VITE_CHAT_URL).href
+  : undefined
